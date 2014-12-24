1e92298f1653762099a8ebd8f78c0979b3b1fb6c
Sarah Kleinman <kleinman.sarah@gmail.com>
2014-12-24 11:02:46 -0500

    moved from js fiddle, modified todo.js

--- todo.js
+++ todo.js
@@ -1,62 +1,104 @@
-//lessons learned
-// - Avoid global variables
-// - Strive to make functions reusable
-// - Don't use getElementbyId in a function, instead
+//Avoid global variables, wrap this in an anon function?
+(function(){
+    var input = document.getElementById("input");
+    var submit = document.getElementById("submit");
+    var list = document.getElementById("list");
+    //create variables here for localStorage to access?
+    //var x = document.getElementById("delete")
+    //var newItem = document.getElementById("...? [array];
 
-function addNewItem(list, itemText){
-// Each item should look like this:
-// <li>
-	//<input type=checkbox />
-	// <span>Add grid to this app</span> 
-//</li>
 
-	var listItem = document.createElement("li")
-	var checkBox = document.createElement("input");
-	checkBox.type = "checkbox";
+    input.focus();
 
-	var span = document.createElement("span");
-	span.textContent = itemText;
-
-	listItem.appendChild(span);
-	listItem.appendChild(checkBox);
-	// listItem.textContent = itemText;
-
-	list.appendChild(listItem);
-}
-//creates an element to add to this list
-//createElement takes a string of the element you want to create
-
-function deleteNewItem(){
-	//function that deletes Item from the DOM
-}
-
-function moveNewItem(){
-	//function that moves item from column to column 
-	//Append to inProgress column
-	//or Append to Delete column
-}
-
-var newItem = document.getElementById("todoList");
-//Reference to the new item
-
-var inItemText = document.getElementById("inItemText");
-inItemText.focus();
-inItemText.onkeyup = function(event) {
-
-	// Event.which (13) -> ENTER
-	if (event.which == 13){
-		var itemText = inItemText.value;
-
-		if (!itemText || itemText == "" || itemText == " ") {
-			return false;
-		}
-
-	addNewItem(document.getElementById("todoList"), itemText);
-	// addNewItem(document.getElementById("inProgress"));
-
-	inItemText.value = " ";
-	}
+/*
+var createButton = function() {
+    var button = 
+    document.createElement('button');
+    button.innerHTML = "X";
+    button.setAttribute("type", "button") 
+    button.setAttribute("id", "delete");
+    console.log(button);
 };
-//Create a function directly here
-//Function with () calls the function immediately. Handler goes to addNewItem function. 
-//to make sure this doesn't fire immediately, wrap it in an anonymouse function. 
+*/
+
+//Add new item
+var createNewItem = function (){   
+    var newTask = document.createElement('li');
+    var button = document.createElement('button');
+    
+    newTask.innerHTML = input.value;
+    newTask.setAttribute("id", list.childNodes.length);
+    newTask.addEventListener("click", deleteItem, false);
+    list.appendChild(newTask);
+   
+    button.innerHTML = 'X';
+    button.setAttribute("id", "delete");
+    button.addEventListener("click", deleteItem, false);
+    newTask.appendChild(button); 
+     
+    //Call localStorage here? Send in what we've just created newTask, button. It will use these values inside the localStorage function. How do I access a function within a function. Put it in global scope - how?
+    
+    //localStorage(newTask);
+    
+    input.value = ''
+    input.focus();
+    
+    
+    //return newTask;
+    //return button;
+    
+};
+
+//localStorage.setItem
+//localStorage.getItem
+
+//delete this node that's inside, find the node in the list with the ID that I know I have. Find out what the ID is, and then delete that from the list. Delete my parent element. 
+
+//Delete item from list //Want to delete button element that's clicked and parent element <li>
+var deleteItem = function(){
+    var listItem = this.parentNode;
+    var ul = listItem.parentNode;
+    
+    ul.removeChild(listItem);
+};
+
+//Create New Item on keypress (Enter) as well as on click
+var keyPress = function(event){
+    var code = event.keyCode;
+    
+    if (code === 13){
+        createNewItem();
+    }
+};
+
+//localStorage - a reference to this function(event) goes inside createItem, with all the instructions outside?
+//Example that helped me think about this
+
+    /*function sayHello(who){
+        documenwrite("Hello," + who);
+    }  
+    
+    sayHello("Bob");
+    */
+    
+function saveTodoItem(createNewItem) {
+    if (localStorage) {
+        var key = "todo" + todoItem.id;
+        var item = JSON.stringify(todoItem);
+        localStorage.setItem(key, item);
+    }
+    else {
+        console.log("Error: you don't have localStorage!");
+    }
+}     
+
+
+document.getElementById("submit").addEventListener('click', createNewItem);
+document.getElementById("input").addEventListener('keypress', keyPress);
+
+}());
+                       
+                       
+//Is the below needed? How does it work without it?
+//document.getElementById("delete").addEventListener('click', deleteItem);
+
